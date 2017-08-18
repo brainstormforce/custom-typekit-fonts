e0ce91b95f7dc1e8612fba07521a304c35337630
Rushi Jagani <rushij@bsf.io>
2017-08-18 15:54:45 +0530

    Removed ajax call & used wp-_remote_get instead

--- classes/class-custom-typekit-fonts-render.php
+++ classes/class-custom-typekit-fonts-render.php
@@ -66,14 +66,14 @@ if ( ! class_exists( 'Custom_Typekit_Fonts_Render' ) ) :
 		 */
 		function typekit_embed_head() {
 			$kit_list = get_option( 'custom-typekit-fonts' );
-			if ( ! isset( $kit_list ) || empty( $kit_list ) ) {
+			if ( empty( $kit_info['custom-typekit-font-details'] ) ) {
 				return;
 			}
 			?>
 			<script>
 			  (function(d) {
 				var config = {
-				  kitId         : '<?php echo $kit_list['kit-id']; ?>',
+				  kitId         : '<?php echo esc_js( $kit_list['custom-typekit-font-id'] ); ?>',
 				  scriptTimeout : 3000,
 				  async         : true
 				},
@@ -93,7 +93,7 @@ if ( ! class_exists( 'Custom_Typekit_Fonts_Render' ) ) :
 		function add_typekit_fonts( $custom_fonts ) {
 
 			$kit_list = get_option( 'custom-typekit-fonts' );
-			$new_custom_fonts = wp_parse_args( $kit_list['kit-list'], $custom_fonts );
+			$new_custom_fonts = wp_parse_args( $kit_list['custom-typekit-font-details'], $custom_fonts );
 
 			return $new_custom_fonts;
 
@@ -108,12 +108,12 @@ if ( ! class_exists( 'Custom_Typekit_Fonts_Render' ) ) :
 		public function render_fonts( $load_fonts ) {
 
 			$kit_list = get_option( 'custom-typekit-fonts' );
-			if ( isset( $kit_list['kit-list'] ) ) {
+			if ( isset( $kit_list['custom-typekit-font-details'] ) ) {
 				foreach ( $load_fonts  as $load_font_name => $load_font ) {
 					$font_arr = explode( ',', $load_font_name );
 					$font_name = $font_arr[0];
 
-					if ( array_key_exists( $font_name , $kit_list['kit-list'] ) ) {
+					if ( array_key_exists( $font_name , $kit_list['custom-typekit-font-details'] ) ) {
 						unset( $load_fonts[ $load_font_name ] );
 					}
 				}
@@ -130,9 +130,9 @@ if ( ! class_exists( 'Custom_Typekit_Fonts_Render' ) ) :
 		public function add_customizer_font_list( $value ) {
 
 			$kit_list = get_option( 'custom-typekit-fonts' );
-			if ( isset( $kit_list['kit-list'] ) ) {
+			if ( isset( $kit_list['custom-typekit-font-details'] ) ) {
 				echo '<optgroup label="Typekit">';
-				foreach ( $kit_list['kit-list'] as $font => $properties ) {
+				foreach ( $kit_list['custom-typekit-font-details'] as $font => $properties ) {
 					echo '<option value="' . esc_attr( $font ) . ',' . $properties['fallback'] . '" ' . selected( $font, $value , false ) . '>' . esc_attr( $font ) . '</option>';
 				}
 			}
