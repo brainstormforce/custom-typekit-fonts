ee1c307277dd63c465a05a582ebfdd329ae9f9d2
ShubhamW <shubhamw@bsf.io>
2020-08-12 17:13:32 +0530

    updated analytics code

--- custom-typekit-fonts.php
+++ custom-typekit-fonts.php
@@ -6,7 +6,7 @@
  * Author:          Brainstorm Force
  * Author URI:      http://www.brainstormforce.com
  * Text Domain:     custom-typekit-fonts
- * Version:         1.0.17
+ * Version:         1.0.18
  *
  * @package         Typekit_Custom_Fonts
  */
@@ -25,7 +25,7 @@ define( 'CUSTOM_TYPEKIT_FONTS_FILE', __FILE__ );
 define( 'CUSTOM_TYPEKIT_FONTS_BASE', plugin_basename( CUSTOM_TYPEKIT_FONTS_FILE ) );
 define( 'CUSTOM_TYPEKIT_FONTS_DIR', plugin_dir_path( CUSTOM_TYPEKIT_FONTS_FILE ) );
 define( 'CUSTOM_TYPEKIT_FONTS_URI', plugins_url( '/', CUSTOM_TYPEKIT_FONTS_FILE ) );
-define( 'CUSTOM_TYPEKIT_FONTS_VER', '1.0.17' );
+define( 'CUSTOM_TYPEKIT_FONTS_VER', '1.0.18' );
 /**
  * BSF Custom Fonts
  */
@@ -40,4 +40,19 @@ if ( is_admin() ) {
 }
 
 // BSF Analytics library.
-require_once CUSTOM_TYPEKIT_FONTS_DIR . 'admin/bsf-analytics/class-bsf-analytics.php';
+if ( ! class_exists( 'BSF_Analytics_Loader' ) ) {
+	require_once CUSTOM_TYPEKIT_FONTS_DIR . 'admin/bsf-analytics/class-bsf-analytics-loader.php';
+}
+
+$bsf_analytics = BSF_Analytics_Loader::get_instance();
+
+$bsf_analytics->set_entity(
+	array(
+		'bsf' => array(
+			'product_name'    => 'Custom Adobe Fonts (Typekit)',
+			'path'            => CUSTOM_TYPEKIT_FONTS_DIR . 'admin/bsf-analytics',
+			'author'          => 'Brainstorm Force',
+			'time_to_display' => '+24 hours',
+		),
+	)
+);
